30238ed2b301383fcbbcd0e4f5ab7c75c7c8f0c9
Lucas Vieira <vieiratm01@hotmail.com>
2024-10-06 19:33:49 -0300

    feat: finished traffic ligth logic

--- index.js
+++ index.js
@@ -16,6 +16,37 @@ const onLigthRed = () => {
     indexRed = (indexRed + 1) % redColors.length;
     if (indexRed == 1) {
       document.querySelector(".traffic-ligth").style.borderColor = redColors[0];
+      document.querySelector(".ligth-min").style.backgroundColor = redColors[0];
+      document.querySelector(".ligth-min-2").style.backgroundColor =
+        redColors[0];
+      document.querySelector(".ligth-min-3").style.backgroundColor =
+        redColors[0];
+      document.querySelector(".ligth-min-4").style.backgroundColor =
+        redColors[0];
+      document.querySelector(".ligth-min-5").style.backgroundColor =
+        redColors[0];
+
+      //Logica de funcionamento do contador vermelho
+      setTimeout(() => {
+        document.querySelector(".ligth-min").style.backgroundColor =
+          borderColor;
+      }, 1600);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-2").style.backgroundColor =
+          borderColor;
+      }, 3200);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-3").style.backgroundColor =
+          borderColor;
+      }, 4800);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-4").style.backgroundColor =
+          borderColor;
+      }, 6400);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-5").style.backgroundColor =
+          borderColor;
+      }, 7900);
     } else {
       document.querySelector(".traffic-ligth").style.borderColor = borderColor;
     }
@@ -30,6 +61,38 @@ const onLigthGren = () => {
     if (indexGren == 1) {
       document.querySelector(".traffic-ligth").style.borderColor =
         grenColors[0];
+      document.querySelector(".ligth-min").style.backgroundColor =
+        grenColors[0];
+      document.querySelector(".ligth-min-2").style.backgroundColor =
+        grenColors[0];
+      document.querySelector(".ligth-min-3").style.backgroundColor =
+        grenColors[0];
+      document.querySelector(".ligth-min-4").style.backgroundColor =
+        grenColors[0];
+      document.querySelector(".ligth-min-5").style.backgroundColor =
+        grenColors[0];
+
+      //Logica de funcionamento do contador verde
+      setTimeout(() => {
+        document.querySelector(".ligth-min").style.backgroundColor =
+          borderColor;
+      }, 1200);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-2").style.backgroundColor =
+          borderColor;
+      }, 2400);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-3").style.backgroundColor =
+          borderColor;
+      }, 3600);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-4").style.backgroundColor =
+          borderColor;
+      }, 4800);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-5").style.backgroundColor =
+          borderColor;
+      }, 5900);
     } else {
       document.querySelector(".traffic-ligth").style.borderColor = borderColor;
     }
@@ -64,8 +127,8 @@ const timer = () => {
         clearTimeout(onLigthYellow());
         onLigthRed();
       }, 2000);
-    }, 4000);
-  }, 6000);
+    }, 6000);
+  }, 8000);
 };
 
 timer();
@@ -73,7 +136,7 @@ timer();
 //Loop da lógica do temporizador
 setInterval(() => {
   timer();
-}, 12000);
+}, 16000);
 
 const onLigthGren2 = () => {
   setTimeout(() => {
@@ -83,6 +146,38 @@ const onLigthGren2 = () => {
     if (indexGren2 == 1) {
       document.querySelector(".traffic-ligth-2").style.borderColor =
         grenColors[0];
+      document.querySelector(".ligth-min-6").style.backgroundColor =
+        grenColors[0];
+      document.querySelector(".ligth-min-7").style.backgroundColor =
+        grenColors[0];
+      document.querySelector(".ligth-min-8").style.backgroundColor =
+        grenColors[0];
+      document.querySelector(".ligth-min-9").style.backgroundColor =
+        grenColors[0];
+      document.querySelector(".ligth-min-10").style.backgroundColor =
+        grenColors[0];
+
+      //Logica de funcionamento contador verde 2
+      setTimeout(() => {
+        document.querySelector(".ligth-min-6").style.backgroundColor =
+          borderColor;
+      }, 1200);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-7").style.backgroundColor =
+          borderColor;
+      }, 2400);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-8").style.backgroundColor =
+          borderColor;
+      }, 3600);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-9").style.backgroundColor =
+          borderColor;
+      }, 4800);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-10").style.backgroundColor =
+          borderColor;
+      }, 5900);
     } else {
       document.querySelector(".traffic-ligth-2").style.borderColor =
         borderColor;
@@ -113,6 +208,38 @@ const onLigthRed2 = () => {
     if (indexRed2 == 1) {
       document.querySelector(".traffic-ligth-2").style.borderColor =
         redColors[0];
+      document.querySelector(".ligth-min-6").style.backgroundColor =
+        redColors[0];
+      document.querySelector(".ligth-min-7").style.backgroundColor =
+        redColors[0];
+      document.querySelector(".ligth-min-8").style.backgroundColor =
+        redColors[0];
+      document.querySelector(".ligth-min-9").style.backgroundColor =
+        redColors[0];
+      document.querySelector(".ligth-min-10").style.backgroundColor =
+        redColors[0];
+
+      //Logica de funcionamento contador vermelho 2
+      setTimeout(() => {
+        document.querySelector(".ligth-min-6").style.backgroundColor =
+          borderColor;
+      }, 1600);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-7").style.backgroundColor =
+          borderColor;
+      }, 3200);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-8").style.backgroundColor =
+          borderColor;
+      }, 4800);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-9").style.backgroundColor =
+          borderColor;
+      }, 6400);
+      setTimeout(() => {
+        document.querySelector(".ligth-min-10").style.backgroundColor =
+          borderColor;
+      }, 7900);
     } else {
       document.querySelector(".traffic-ligth-2").style.borderColor =
         borderColor;
@@ -133,9 +260,9 @@ const timer2 = () => {
       setTimeout(() => {
         clearTimeout(onLigthRed2());
         onLigthGren2();
-      }, 6000);
+      }, 8000);
     }, 2000);
-  }, 4000);
+  }, 6000);
 };
 
 timer2();
@@ -143,4 +270,4 @@ timer2();
 //Loop da lógica do temporizador
 setInterval(() => {
   timer2();
-}, 12000);
+}, 16000);
